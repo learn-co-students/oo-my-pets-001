85d78a8cbdfe852b8068e7bc33aebe14fe97df97
fhernandez173 <f.hernandez173@gmail.com>
2016-06-28 14:23:07 -0400

    challenging.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+
+  attr_accessor :mood
+  attr_reader :dog_name
+
+  def initialize(dog_name)
+    @dog_name = dog_name
+    @mood = "nervous"
+  end
+
+  def name
+    @dog_name
+  end
+
 end
+
+Dog.new("Daisy")
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,17 @@
 class Fish
-  # code goes here
+
+  attr_accessor :mood
+  attr_reader :fish_name
+
+  def initialize(fish_name)
+    @fish_name = fish_name
+    @mood = "nervous"
+  end
+
+  def name
+    @fish_name
+  end
+
 end
+
+Fish.new("Nemo")
